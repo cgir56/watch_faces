b883a936cb4657e463136f7ec2acff149d54355b
cgir56 <32414607+cgir56@users.noreply.github.com>
2023-12-15 22:31:29 -0500

    Added watch face editor files

---
+++ File_templates/index.js
@@ -0,0 +1,54 @@
+    /*
+    ** Watch_Face_Editor tool
+    ** watchface js version v2.1.1
+    ** Copyright © SashaCX75. All Rights Reserved
+    */
+   
+    try {
+    (() => {
+        //start of ignored block
+        const __$$app$$__ = __$$hmAppManager$$__.currentApp;
+        function getApp() {
+            return __$$app$$__.app;
+        }
+        function getCurrentPage() {
+            return __$$app$$__.current && __$$app$$__.current.module;
+        }
+        const __$$module$$__ = __$$app$$__.current;
+        const h = new DeviceRuntimeCore.WidgetFactory(new DeviceRuntimeCore.HmDomApi(__$$app$$__, __$$module$$__));
+        const {px} = __$$app$$__.__globals__;
+        const logger = Logger.getLogger('watchface_SashaCX75');
+        //end of ignored block
+
+        //dynamic modify start
+
+        //Variable declaration section
+
+        //dynamic modify end
+
+        __$$module$$__.module = DeviceRuntimeCore.WatchFace({
+            init_view() {
+                //dynamic modify start
+                    
+                //Item description section
+
+                //dynamic modify end
+            },
+            onInit() {
+                logger.log('index page.js on init invoke');
+            },
+            build() {
+                this.init_view();
+                logger.log('index page.js on ready invoke');
+            },
+            onDestroy() {
+                logger.log('index page.js on destroy invoke');
+            }
+        });
+        ;
+    })();
+} catch (e) {
+    console.log('Mini Program Error', e);
+    e && e.stack && e.stack.split(/\n/).forEach(i => console.log('error stack', i));
+    ;
+}
